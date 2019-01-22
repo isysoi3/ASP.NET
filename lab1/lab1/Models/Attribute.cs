05dcd5d863993830c46d94ec306d7eb23df27239
Ilya Sysoi <ilya.sysoi@gmail.com>
2019-01-22 23:01:58 +0300

    added all labs

---
+++ lab1/lab1/Models/Attribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab1.Models
+{
+    public class Attribute
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+    }
+}
---
+++ lab1/lab1/Models/TableModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab1.Models
+{
+    public class TableModel
+    {
+        public string Name { get; set; }
+        public int NumberOfAttributes { get; set; }
+        public IEnumerable<Attribute> Attributes { get; set; }
+    }
+}
